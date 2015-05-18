a7b44166313eeb373f92926d1c879680f4893b54
FabDesigner <fabianodesenvolvedor@outlook.com>
2015-05-18 17:31:30 -0300

    Minha primeira calculadora de soma e média de notas.

---
+++ CalcularNotas/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CalcularNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.txtNota5 = new System.Windows.Forms.TextBox();
+            this.txtNota6 = new System.Windows.Forms.TextBox();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.txtSoma = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota 1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota 3:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota 4:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nota 5:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nota 6:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Soma das notas:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(100, 222);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Média das notas:";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota1.Location = new System.Drawing.Point(64, 17);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(50, 20);
+            this.txtNota1.TabIndex = 8;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota2.Location = new System.Drawing.Point(64, 44);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(50, 20);
+            this.txtNota2.TabIndex = 9;
+            this.txtNota2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota3.Location = new System.Drawing.Point(65, 71);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(50, 20);
+            this.txtNota3.TabIndex = 10;
+            this.txtNota3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota4.Location = new System.Drawing.Point(64, 98);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(50, 20);
+            this.txtNota4.TabIndex = 11;
+            this.txtNota4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtNota5
+            // 
+            this.txtNota5.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota5.Location = new System.Drawing.Point(65, 125);
+            this.txtNota5.Name = "txtNota5";
+            this.txtNota5.Size = new System.Drawing.Size(50, 20);
+            this.txtNota5.TabIndex = 12;
+            this.txtNota5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txtNota6
+            // 
+            this.txtNota6.BackColor = System.Drawing.Color.Yellow;
+            this.txtNota6.Location = new System.Drawing.Point(64, 152);
+            this.txtNota6.Name = "txtNota6";
+            this.txtNota6.Size = new System.Drawing.Size(50, 20);
+            this.txtNota6.TabIndex = 13;
+            this.txtNota6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(191, 219);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.ReadOnly = true;
+            this.txtMedia.Size = new System.Drawing.Size(50, 20);
+            this.txtMedia.TabIndex = 15;
+            // 
+            // txtSoma
+            // 
+            this.txtSoma.Location = new System.Drawing.Point(192, 192);
+            this.txtSoma.Name = "txtSoma";
+            this.txtSoma.ReadOnly = true;
+            this.txtSoma.Size = new System.Drawing.Size(50, 20);
+            this.txtSoma.TabIndex = 14;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Red;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Location = new System.Drawing.Point(176, 153);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 16;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            this.btnCalcular.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CalcularNotas.Properties.Resources.amiga;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.txtSoma);
+            this.Controls.Add(this.txtNota6);
+            this.Controls.Add(this.txtNota5);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.TextBox txtNota5;
+        private System.Windows.Forms.TextBox txtNota6;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.TextBox txtSoma;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ CalcularNotas/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "clicou";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtSoma.Text = Convert.ToString(Convert.ToDecimal(txtNota1.Text) + Convert.ToDecimal(txtNota2.Text) +
+                                            Convert.ToDecimal(txtNota3.Text) + Convert.ToDecimal(txtNota4.Text) +
+                                            Convert.ToDecimal(txtNota5.Text) + Convert.ToDecimal(txtNota6.Text));
+
+            txtMedia.Text = Convert.ToString((Convert.ToDecimal(txtNota1.Text) + Convert.ToDecimal(txtNota2.Text) +
+                                            Convert.ToDecimal(txtNota3.Text) + Convert.ToDecimal(txtNota4.Text) +
+                                            Convert.ToDecimal(txtNota5.Text) + Convert.ToDecimal(txtNota6.Text)) / 6);
+
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+            txtNota4.Text = "";
+            txtNota5.Text = "";
+            txtNota6.Text = "";
+            txtNota1.Focus();
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
